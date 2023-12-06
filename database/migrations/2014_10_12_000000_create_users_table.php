7d25ab7637ac190cdcbd2b0a486c3c77259410d7
hoangnh <hoangnh@regit-technology.com>
2023-12-06 09:35:37 +0700

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             'users',
             function (Blueprint $table) {
                 $table->id();
-                $table->string('name', 255);
+                $table->string('name', 255)->unique();
                 $table->string('email', 255)->unique();
                 $table->string('password', 255);
                 $table->string('image', 255)->nullable();
